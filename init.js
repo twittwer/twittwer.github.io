6d03c9ec46fe5b19b7a652614f1a2aecbd12ee7c
Tobias Wittwer <tobias.wittwer@hpe.com>
2017-06-12 10:12:11 +0200

    [FEATURE] adding birthday and some wording

--- init.js
+++ init.js
@@ -1,16 +1,31 @@
 'use strict';
 
-$( document )
-  .ready( function () {
+function calcAge(birthday, dateToCalculate) {
+  dateToCalculate = (dateToCalculate || (dateToCalculate instanceof Date)) ? dateToCalculate : new Date();
+  if (!birthday || !(birthday instanceof Date)) {
+    throw new Error('Invalid birthday parameter! - Necessary and needs to be a Date.');
+  }
+  var age = dateToCalculate.getFullYear() - birthday.getFullYear();
+  birthday.setFullYear(dateToCalculate.getFullYear());
+  if (dateToCalculate.getTime() < birthday.getTime()) {
+    age = age - 1;
+  }
+  return age;
+}
 
-    $( '.button-collapse' )
+$(document)
+  .ready(function () {
+
+    $('.button-collapse')
       .sideNav();
 
-    $( '.dropdown-button' )
-      .dropdown( {
+    $('.dropdown-button')
+      .dropdown({
         constrainWidth: false,
-        hover         : false,
-        belowOrigin   : true
-      } );
+        hover: false,
+        belowOrigin: true
+      });
 
-  } );
+    document.getElementById('birthday-age').innerText = '(' + calcAge(new Date(1995, 8, 16)) + ')';
+
+  });
